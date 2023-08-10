f868faa72a41cc94ff5558e40c07a29817e09af2
falashlion <forchufelix@gmail.com>
2023-08-10 06:23:13 +0000

    final

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,28 @@
-#!/usr/bin/python3
-""" raddit api"""
-
 import json
 import requests
 
-
-def count_words(subreddit, word_list, after="", count=[]):
-    """count all words"""
-
-    if after == "":
+def count_words(subreddit, word_list, after=None, count=None):
+    if count is None:
         count = [0] * len(word_list)
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'user-agent': 'bhalut'})
-
-    if request.status_code == 200:
-        data = request.json()
-
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
-
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
-
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
-
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
+    if after is None:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    else:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    headers = {'User-Agent': 'bhalut'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for topic in data['data']['children']:
+            title = topic['data']['title']
+            for word in title.split():
+                word = word.lower().strip('.,!_?')
+                if word in word_list:
+                    count[word_list.index(word)] += 1
+        next_page = data['data']['after']
+        if next_page is None:
+            word_count = [(word_list[i], count[i]) for i in range(len(word_list)) if count[i] > 0]
+            word_count.sort(key=lambda x: (-x[1], x[0]))
+            for word, count in word_count:
+                print(f"{word}: {count}")
         else:
-            count_words(subreddit, word_list, after, count)
+            count_words(subreddit, word_list, next_page, count)
